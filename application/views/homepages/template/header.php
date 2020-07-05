cf044302b78bd85f4b9b9a4b044aa41e24651c31
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-07-05 16:18:44 +0700

    add

--- application/views/homepages/template/header.php
+++ application/views/homepages/template/header.php
@@ -67,6 +67,9 @@
   <link href="https://demo.jantra.co/AssetsHome/vendor/aos/aos.css" rel="stylesheet">
 
  <script src="https://code.jquery.com/jquery-3.5.1.js" ></script>
+ <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+ <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+ 
 
   <!-- Template Main CSS File -->
   <link href="https://demo.jantra.co/AssetsHome/css/style.css" rel="stylesheet">
