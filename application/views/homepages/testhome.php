a525267ff597f154f5465ee790d502474548bb1c
jobsmcthai <edp-kittikarn.p@mail.smcthai.co.th>
2020-07-05 21:41:06 +0700

    TEST

--- application/views/homepages/testhome.php
+++ application/views/homepages/testhome.php
@@ -1953,13 +1953,28 @@ html{font-family:sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:1
         </script>
 
 
+    
+    <script type="text/javascript" src="../assets/plugins/jquery/jquery.min.js"></script> 
+    <script type="text/javascript" src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>  
+    <script type="text/javascript" src="../assets/plugins/fancybox/source/jquery.fancybox.pack.js"></script> 
+    <script type="text/javascript" src="../assets/js/app.js"></script>
+    <script type="text/javascript" src="../assets/js/plugins/fancy-box.js"></script> 
+    <!--- cude portfolio--> 
+    
+  
+
+        <script type="text/javascript" src="../assets/plugins/owl-carousel/owl-carousel/owl.carousel.js"></script>
+        <script type="text/javascript" src="../assets/js/plugins/owl-carousel.js"></script>
+
+
 
     <script type="text/javascript" src="https://demo.jantra.co/AssetsLogo/plugins/jquery.min.js"></script> 
     <script type="text/javascript" src="https://demo.jantra.co/AssetsLogo/plugins/bootstrap.min.js"></script>  
     <script type="text/javascript" src="https://demo.jantra.co/AssetsLogo/plugins/jquery.fancybox.pack.js"></script> 
     <script type="text/javascript" src="https://demo.jantra.co/AssetsLogo/plugins/app.js"></script>
     <script type="text/javascript" src="https://demo.jantra.co/AssetsLogo/plugins/fancy-box.js"></script>  
-        <script type="text/javascript" src="https://demo.jantra.co/AssetsLogo/plugins/owl.carousel.js"></script> 
+    <script type="text/javascript" src="https://demo.jantra.co/AssetsLogo/plugins/owl.carouseled.js"></script> 
+    <script type="text/javascript" src="https://demo.jantra.co/AssetsLogo/plugins/owl.carousel.js"></script> 
 
  <!-- LISIT  ADD  
 
