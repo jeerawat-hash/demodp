7809f482bdfe13aa6f96d0f63bc10e858325c417
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-07-05 16:00:55 +0700

    add

--- application/views/homepages/noticenews.php
+++ application/views/homepages/noticenews.php
@@ -1,6 +1,11 @@
 
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css"> 
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script
+  src="https://code.jquery.com/jquery-2.2.4.min.js"
+  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+  crossorigin="anonymous"></script>
+
 
  <main id="main">
 
