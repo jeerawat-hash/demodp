e4aeebfcf44e5bc6ed26c2dae8a7ab61e28d2e6d
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-07-05 11:22:18 +0700

    ada

---
+++ application/views/management/blank.php
@@ -0,0 +1,24 @@
+
+<!--- content -----> 
+    <section class="content">
+
+ 
+        <div class="container-fluid">
+            <div class="block-header">
+                <h2>Blank Page</h2>
+            </div>
+        
+        </div>
+
+
+
+
+
+
+
+
+
+    </section>
+
+
+<!--- content ----->
