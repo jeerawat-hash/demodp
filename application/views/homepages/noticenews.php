8b37bd49b13e424f1df532679e1f4458198ef581
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-07-05 16:45:16 +0700

    add

--- application/views/homepages/noticenews.php
+++ application/views/homepages/noticenews.php
@@ -20,35 +20,7 @@
 
 
             
-            <article class="entry" data-aos="fade-up">
-
-              <div class="entry-img">
-                <img src="https://demo.jantra.co/AssetsHome/img/blog-4.jpg" alt="" class="img-fluid">
-              </div>
-
-              <h2 class="entry-title">
-                <a href="blog-single.html">Non rem rerum nam cum quo minus. Dolor distinctio deleniti explicabo eius exercitationem. Veniam eius velit ab ipsa quidem rem.</a>
-              </h2>
-
-              <div class="entry-meta">
-                <ul>
-                  <li class="d-flex align-items-center"><i class="icofont-user"></i> <a href="blog-single.html">John Doe</a></li>
-                  <li class="d-flex align-items-center"><i class="icofont-wall-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a></li>
-                  <li class="d-flex align-items-center">
-                </ul>
-              </div>
-
-              <div class="entry-content">
-                <p>
-                  Aspernatur rerum perferendis et sint. Voluptates cupiditate voluptas atque quae. Rem veritatis rerum enim et autem. Saepe atque cum eligendi eaque iste omnis a qui.
-                  Quia sed sunt. Ea asperiores expedita et et delectus voluptates rerum. Id saepe ut itaque quod qui voluptas nobis porro rerum. Quam quia nesciunt qui aut est non omnis. Inventore occaecati et quaerat magni itaque nam voluptas. Voluptatem ducimus sint id earum ut nesciunt sed corrupti nemo.
-                </p>
-                <div class="read-more">
-                  <a href="blog-single.html">Read More</a>
-                </div>
-              </div>
-
-            </article><!-- End blog entry -->
+           <div id="NewsContent"></div>
 
 
           </div><!-- End blog entries list -->
@@ -82,10 +54,50 @@
   <script type="text/javascript">
     
     $(function(){
-  
+      var htmlall = "";
+      var html = '<article class="entry" data-aos="fade-up">'+
+              '<div class="entry-img">'+
+                '<img src="https://demo.jantra.co/AssetsHome/img/blog-4.jpg" alt="" class="img-fluid">'+
+              '</div>'+
+              '<h2 class="entry-title">'+
+              '<a href="blog-single.html">Non rem rerum nam cum quo minus. Dolor distinctio deleniti explicabo eius exercitationem. Veniam eius velit ab ipsa quidem rem.</a>'+
+              '</h2>'+
+              '<div class="entry-meta">'+
+                '<ul>'+
+                  '<li class="d-flex align-items-center"><i class="icofont-user"></i> <a href="blog-single.html">John Doe</a></li>'+
+                  '<li class="d-flex align-items-center"><i class="icofont-wall-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a></li>'+
+                  '<li class="d-flex align-items-center">'+
+                '</ul>'+
+              '</div>'+
+              '<div class="entry-content">'+
+               ' <p>'+
+               '  Aspernatur rerum perferendis et sint. Voluptates cupiditate voluptas atque quae. Rem veritatis rerum enim et autem. Saepe atque cum eligendi eaque iste omnis a qui.'+
+                '  Quia sed sunt. Ea asperiores expedita et et delectus voluptates rerum. Id saepe ut itaque quod qui voluptas nobis porro rerum. Quam quia nesciunt qui aut est non omnis. Inventore occaecati et quaerat magni itaque nam voluptas. Voluptatem ducimus sint id earum ut nesciunt sed corrupti nemo.'+
+                '</p>'+
+                '<div class="read-more">'+
+                  '<a href="blog-single.html">Read More</a>'+
+                '</div>'+
+              '</div>'+
+            '</article>';
+
+      for (var i = 0; i < 3; i++) {
+          htmlall += html;
+      }
+
+
+      $("#NewsContent").html(htmlall);
 
       $(window).on("scroll",function(){
-        alert("scroll");
+
+        //alert("scroll");
+        var htmlplus = "";
+        for (var i = 0; i < 1; i++) {
+          htmlplus += html;
+        }
+
+        $("#NewsContent").append(htmlplus);
+
+
       })
 
 
