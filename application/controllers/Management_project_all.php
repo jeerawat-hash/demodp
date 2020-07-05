01bb37efc22de4427823575225ed3827ace0b79e
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-07-05 11:37:12 +0700

    fds

---
+++ application/controllers/Management_project_all.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Management_project_all extends CI_Controller {
+ 
+	function __construct()
+	{
+	
+ 		parent::__construct(); 
+ 		/*$this->load->library("session");
+		$this->load->model("Member_model");
+
+		if ( $this->session->userdata("memberID") == "" ) {
+			
+			redirect("signin");
+			$this->session->sess_destroy();
+		}
+		*/
+		
+
+	}
+
+
+	public function index()
+	{
+		$header['page_name'] = 'โครงการ';
+		$header['page_focus'] = 'project';
+		$header['page_menu'] = 1;
+
+		$this->load->view('management/template/header',$header);
+		$this->load->view('management/blank');
+		$this->load->view('management/template/footer');
+	}
+
+
+
+}
