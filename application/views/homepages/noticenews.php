90577ab0dbaab3bf46cf5b2586b6c714e905f3c2
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-07-05 15:58:17 +0700

    add

--- application/views/homepages/noticenews.php
+++ application/views/homepages/noticenews.php
@@ -4,7 +4,7 @@
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-
+//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js
  <main id="main">
 
     <!-- ======= Breadcrumbs ======= -->
@@ -82,10 +82,9 @@
 
 
   <script type="text/javascript">
-    $(document).ready(function() {
-    $('#example').DataTable( {
-        "order": [[ 3, "desc" ]]
+    
+    $(document).ready( function () {
+    $('#example').DataTable();
     } );
-} );
 
   </script>
