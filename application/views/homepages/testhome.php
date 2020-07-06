fc60f7f6e57925efdcd3d3ef1c005bfec3f8f594
jobsmcthai <edp-kittikarn.p@mail.smcthai.co.th>
2020-07-06 16:23:21 +0700

    sadsasd

--- application/views/homepages/testhome.php
+++ application/views/homepages/testhome.php
@@ -818,25 +818,23 @@
 
           <div class="row portfolio-container" data-aos="fade-up">
                 
-                 <div class="col-sm-12  col-md-12 col-lg-12  filter-app"  >
+                 <div class="col-sm-12  col-md-12 col-lg-12 portfolio-item  "  >
             
-                  <div class="owl-carousel owl-theme" id="7" >
+                  <div class="owl-carousel owl-theme filter-app" id="7"  >
                    <div class="item"  style="width: 100px;  "  data-hash="two"><img src="https://demo.jantra.co/AssetsHome/img/slidehome/2.png"   ><h4>1</h4></div> 
 
                     <div class="item"  style="width: 100px;  "   data-hash="two"><img src="https://demo.jantra.co/AssetsHome/img/slidehome/2.png"     ><h4>2</h4></div>
                      
                   </div>
-                </div>
 
-                   <div class="col-sm-12  col-md-12 col-lg-12   filter-card"  >
-            
-                  <div class="owl-carousel owl-theme" id="7" >
+                  
+                  <div class="owl-carousel owl-theme filter-card" id="7"  >
                    <div class="item"  style="width: 100px;  "  data-hash="two"><img src="https://demo.jantra.co/AssetsHome/img/slidehome/2.png"   ><h4>1</h4></div> 
 
                     <div class="item"  style="width: 100px;  "   data-hash="two"><img src="https://demo.jantra.co/AssetsHome/img/slidehome/2.png"     ><h4>2</h4></div>
                      
                   </div>
-                
+
               </div> 
 
 
