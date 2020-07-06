24b282c89e220c903eb6c527b771fbb7ab67a38f
jobsmcthai <edp-kittikarn.p@mail.smcthai.co.th>
2020-07-06 15:31:10 +0700

    ssdsd

--- application/views/homepages/testhome.php
+++ application/views/homepages/testhome.php
@@ -837,10 +837,10 @@
                      
 
 
-                    <div class="item"  data-hash="two"><img src="https://demo.jantra.co/AssetsHome/img/slidehome/2.png" height="150px" width="30px"   ><h4>9</h4></div>
-                      <div class="item"  data-hash="zero"><img src="https://demo.jantra.co/AssetsHome/img/slidehome/1.png"  height="150px" width="30px" > <h4>10</h4></div>
+                    <div class="item"  data-hash="two"><img src="https://demo.jantra.co/AssetsHome/img/slidehome/2.png" height="150px" width="10px"   ><h4>9</h4></div>
+                      <div class="item"  data-hash="zero"><img src="https://demo.jantra.co/AssetsHome/img/slidehome/1.png"  height="150px" width="15px" > <h4>10</h4></div>
 
-                    <div class="item"  data-hash="two"><img src="https://demo.jantra.co/AssetsHome/img/slidehome/2.png"  height="150px" width="30px"   ><h4>11</h4></div>
+                    <div class="item"  data-hash="two"><img src="https://demo.jantra.co/AssetsHome/img/slidehome/2.png"  height="150px" width="20px"   ><h4>11</h4></div>
                      
                   </div>
                
