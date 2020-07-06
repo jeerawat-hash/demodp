21652659e3d651bea2ff96ffcdac75420570c735
jobsmcthai <edp-kittikarn.p@mail.smcthai.co.th>
2020-07-06 13:03:55 +0700

    test

--- application/views/homepages/testhome.php
+++ application/views/homepages/testhome.php
@@ -812,20 +812,42 @@
           <div class="owl-carousel owl-theme">
 
 
-            <div class="item" data-hash="zero">
-              <h4>0</h4>
-            </div>
-            <div class="item" data-hash="one">
+           <div class="owl-carousel owl-theme">
+            <div class="item">
               <h4>1</h4>
             </div>
-            <div class="item" data-hash="two">
+            <div class="item">
               <h4>2</h4>
             </div>
-            <div class="item" data-hash="three">
+            <div class="item">
               <h4>3</h4>
             </div>
-            <div class="item" data-hash="four">
+            <div class="item">
               <h4>4</h4>
+            </div>
+            <div class="item">
+              <h4>5</h4>
+            </div>
+            <div class="item">
+              <h4>6</h4>
+            </div>
+            <div class="item">
+              <h4>7</h4>
+            </div>
+            <div class="item">
+              <h4>8</h4>
+            </div>
+            <div class="item">
+              <h4>9</h4>
+            </div>
+            <div class="item">
+              <h4>10</h4>
+            </div>
+            <div class="item">
+              <h4>11</h4>
+            </div>
+            <div class="item">
+              <h4>12</h4>
             </div>
          
           </div>
