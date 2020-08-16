44cb06e1969981d71ec08fce77dbf2dd52d824d0
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-08-16 22:05:03 +0700

    add

--- application/views/homepages/org.php
+++ application/views/homepages/org.php
@@ -13,7 +13,7 @@
       <div class="container">
 
         <div class="d-flex justify-content-between align-items-center">
-          <h2>โครงสร้างองค์กร</h2>
+          <h2>ทำเนียบ ศพอ.พระประแดง</h2>
         </div>
 
       </div>
