e4aad6bb23adacda51584bbc8eb81769099be59e
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-08-16 21:55:44 +0700

    add

--- application/views/homepages/org.php
+++ application/views/homepages/org.php
@@ -21,22 +21,49 @@
 
  
 
+
+
     <!-- ======= Our Team Section ======= -->
-    <section id="team" class="team section-bg">
+    <section id="team" class="team">
       <div class="container">
 
-        <div class="section-title" data-aos="fade-up">
-          <p> <strong>คำอธิบาย</strong></p>
-        </div>
+        
 
         <div class="row">
-          <center>
-          <div id="tree"></div>
-          </center>
+  
+
+          <div class="col-lg-12 col-md-6 align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="200">
+              <div class="member-img">
+                <img src="https://demo.jantra.co/AssetsHome/img/orgemp.png" class="img-fluid" alt="">
+               <!-- <div class="social">
+                  
+                  <a href=""><i class="icofont-twitter"></i></a>
+                  <a href=""><i class="icofont-facebook"></i></a>
+                  <a href=""><i class="icofont-instagram"></i></a>
+                  <a href=""><i class="icofont-linkedin"></i></a>
+
+                </div>-->
+              </div>
+              <div class="member-info">
+                <h4>ทำเนียบ ศพอ.พระประแดง</h4>
+                <!--<span>CTO</span> -->
+              </div>
+            </div>
+          </div>
+ 
+
         </div>
+ 
+
 
       </div>
     </section><!-- End Our Team Section -->
+ 
+
+
+
+
  
     <!-- ======= Our Clients Section ======= -->
     <section id="clients" class="clients">
