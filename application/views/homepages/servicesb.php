6981b3c68268512032ca14ce926dffb592410663
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-08-17 14:56:31 +0700

    add

--- application/views/homepages/servicesb.php
+++ application/views/homepages/servicesb.php
@@ -25,23 +25,29 @@
 
 
           <div class="section-title" data-aos="fade-up">
-                <img src="https://demo.jantra.co/AssetsHome/img/service1.png" alt="" class="img-fluid">
-          <h2>Some <strong>Features</strong> we do provide</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                <img src="https://demo.jantra.co/AssetsHome/img/service1.png" width="40%" alt="" class="img-fluid">
+          <h2>ศูนย์บริการคนพิการจังหวัดสมุทรปราการ</h2>
+          <p>กรมส่งเสริมและพัฒนาคุณภาพชีวิตคนพิการ กำหนดให้หน่วยนงานในสังกัดกรมฯ จำนวน 12 แห่งทั่วประเทศ จัดตั้งเป็นศูนย์บริการคนพิการจังหวัดสาขา เพื่อสนับสนุนการดำเนินงานของศูนย์บริการคนพิการจังหวัด ดำเนินการในลักษณะ .ศูนย์บริการใกล้ตัวใกล้ใจสำหรับคนพิการ. เพื่อสนับสนุนให้คนพิการได้เข้าถึงสิทธิที่พึงจะได้รับตามกฏหมาย</p>
+          <h2><strong>บริการ</strong></h2>
+          <p>
+            <br>1.ออกบัตรประจำตัวคนพิการ / ต่ออายุบัตร
+            <br>2.รับยื่นเอกสารการกู้ยืมเงินประกอบอาชีพ
+            <br>3.ให้บริการเครื่องบริการถ่ายทอดการสื่อสารสาธารณะ (TTRS) กับคนพิการทางการได้ยิน
+            <br>4.ให้คำแนะนำเรื่องสิทธิคนพิการ / การจ้างงานคนพิการ / ประสานส่งต่อให้ความช่วยเหลือ
+            <br>5.จำหน่ายผลิตภัณฑ์ของคนพิการ
+          </p>
         </div>
 
 
           <div class="col-lg-12 entries">
 
-
-            
-           
+      
            <article class="entry" data-aos="fade-up">
               <div class="entry-img">
                 <img src="https://demo.jantra.co/AssetsHome/img/blog-4.jpg" alt="" class="img-fluid">
               </div>
               <h2 class="entry-title">
-              <a href="blog-single.html">ศูนย์บริการคนพิการสาขา.</a>
+              <a href="blog-single.html">เอกสารที่ใช้ในการขอมีบัตรประจำตัวคนพิการ.</a>
               </h2>
               <div class="entry-meta">
                 <ul>
@@ -51,7 +57,11 @@
                 </ul>
               </div>
               <div class="entry-content">
-                <p>เนื่อหาโครงการ
+                <p>
+                  <br>1.บัตรประชาชน หรือบัตรประจำตัวข้าราชการ หรือสูติบัตรสำหรับบุคคลอายุต่ำกว่า 7 ปี หรือหนังสือรับรองการเกิดตามแบบที่กรมการปกครองกำหนด
+                  <br>2.ทะเบียนบ้านของคนพิการ
+                  <br>3.รูปถ่ายขนาด 1 นิ้ว ถ่ายไว้ไม่เกิน 6 เดือน จำนวน 2 รูป
+                  <br>4.เอกสารรับรองความพิการ โดยผู้ประกอบวิชาชีพเวชกรรมของสถานพยาบาลของรัฐ
                 </p>
                 <div class="read-more">
                   <a href="https://demo.jantra.co/index.php/Home/projectdetail">รายละเอียด</a>
