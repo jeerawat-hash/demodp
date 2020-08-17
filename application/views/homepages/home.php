347ac5feaed68ff9930dbce8a657dba942e2d572
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-08-17 15:41:58 +0700

    add

--- application/views/homepages/home.php
+++ application/views/homepages/home.php
@@ -541,113 +541,103 @@
     <!-- ======= Portfolio Section ======= -->
     <section id="portfolio" class="portfolio">
       <div class="container">
-
+        
         <div class="section-title" data-aos="fade-up">
-          <h2>โครงการตัวอย่าง</h2>
+          <h2>ผลิตภัณฑ์ฝีมือคนพิการ</h2>
         </div>
 
         <div class="row" data-aos="fade-up">
           <div class="col-lg-12 d-flex justify-content-center">
             <ul id="portfolio-flters">
-              <li data-filter="*" class="filter-active">All</li>
-              <li data-filter=".filter-app">App</li>
-              <li data-filter=".filter-card">Card</li>
-              <li data-filter=".filter-web">Web</li>
+              <li data-filter="*" class="filter-active">ทั้งหมด</li>
+              <li data-filter=".filter-bag">กระเป๋า</li>
+              <li data-filter=".filter-cur">อุปกรณ์ตกแต่ง</li> 
             </ul>
           </div>
         </div>
 
         <div class="row portfolio-container" data-aos="fade-up">
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            <img src="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-bag">
+            <img src="https://demo.jantra.co/AssetsHome/img/bag1.png" class="img-fluid" alt="">
             <div class="portfolio-info">
-              <h4>App 1</h4>
-              <p>App</p>
-              <a href="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-1.jpg" data-gall="portfolioGallery" class="venobox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
+              <h4>กระเป๋าแฟชั่น</h4>
+              <p>กระเป๋าแฟชั่น</p>
+              <a href="https://demo.jantra.co/AssetsHome/img/bag1.png" data-gall="portfolioGallery" class="venobox preview-link" title="กระเป๋าแฟชั่น"><i class="bx bx-plus"></i></a>
               <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <img src="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-bag">
+            <img src="https://demo.jantra.co/AssetsHome/img/bag2.png" class="img-fluid" alt="">
             <div class="portfolio-info">
-              <h4>Web 3</h4>
-              <p>Web</p>
-              <a href="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-2.jpg" data-gall="portfolioGallery" class="venobox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
+              <h4>กระเป๋าใส่</h4>
+              <p>กระเป๋าใส่</p>
+              <a href="https://demo.jantra.co/AssetsHome/img/bag2.png" data-gall="portfolioGallery" class="venobox preview-link" title="กระเป๋าใส่"><i class="bx bx-plus"></i></a>
               <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            <img src="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-bag">
+            <img src="https://demo.jantra.co/AssetsHome/img/bag3.png" class="img-fluid" alt="">
             <div class="portfolio-info">
-              <h4>App 2</h4>
-              <p>App</p>
-              <a href="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-3.jpg" data-gall="portfolioGallery" class="venobox preview-link" title="App 2"><i class="bx bx-plus"></i></a>
+              <h4>กระเป๋าอินดี้</h4>
+              <p>กระเป๋าอินดี้</p>
+              <a href="https://demo.jantra.co/AssetsHome/img/bag3.png" data-gall="portfolioGallery" class="venobox preview-link" title="กระเป๋าอินดี้"><i class="bx bx-plus"></i></a>
               <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-            <img src="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-bag">
+            <img src="https://demo.jantra.co/AssetsHome/img/bag4.png" class="img-fluid" alt="">
             <div class="portfolio-info">
-              <h4>Card 2</h4>
-              <p>Card</p>
-              <a href="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-4.jpg" data-gall="portfolioGallery" class="venobox preview-link" title="Card 2"><i class="bx bx-plus"></i></a>
+              <h4>กระเป๋ารูปหมี</h4>
+              <p>กระเป๋ารูปหมี</p>
+              <a href="https://demo.jantra.co/AssetsHome/img/bag4.png" data-gall="portfolioGallery" class="venobox preview-link" title="กระเป๋ารูปหมี"><i class="bx bx-plus"></i></a>
               <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <img src="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-bag">
+            <img src="https://demo.jantra.co/AssetsHome/img/bag5.png" class="img-fluid" alt="">
             <div class="portfolio-info">
-              <h4>Web 2</h4>
-              <p>Web</p>
-              <a href="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-5.jpg" data-gall="portfolioGallery" class="venobox preview-link" title="Web 2"><i class="bx bx-plus"></i></a>
+              <h4>กระเป๋ารูปหมี</h4>
+              <p>กระเป๋ารูปหมี</p>
+              <a href="https://demo.jantra.co/AssetsHome/img/bag5.png" data-gall="portfolioGallery" class="venobox preview-link" title="กระเป๋ารูปหมี"><i class="bx bx-plus"></i></a>
               <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            <img src="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-cur">
+            <img src="https://demo.jantra.co/AssetsHome/img/cur1.png" class="img-fluid" alt="">
             <div class="portfolio-info">
-              <h4>App 3</h4>
-              <p>App</p>
-              <a href="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-6.jpg" data-gall="portfolioGallery" class="venobox preview-link" title="App 3"><i class="bx bx-plus"></i></a>
+              <h4>สายรัดข้อมือหนัง</h4>
+              <p>สายรัดข้อมือหนัง</p>
+              <a href="https://demo.jantra.co/AssetsHome/img/cur1.png" data-gall="portfolioGallery" class="venobox preview-link" title="สายรัดข้อมือหนัง"><i class="bx bx-plus"></i></a>
               <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-            <img src="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-cur">
+            <img src="https://demo.jantra.co/AssetsHome/img/cur2.png" class="img-fluid" alt="">
             <div class="portfolio-info">
-              <h4>Card 1</h4>
-              <p>Card</p>
-              <a href="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-7.jpg" data-gall="portfolioGallery" class="venobox preview-link" title="Card 1"><i class="bx bx-plus"></i></a>
+              <h4>พวงกุญแจหนัง</h4>
+              <p>พวงกุญแจหนัง</p>
+              <a href="https://demo.jantra.co/AssetsHome/img/cur2.png" data-gall="portfolioGallery" class="venobox preview-link" title="พวงกุญแจหนัง"><i class="bx bx-plus"></i></a>
               <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-            <img src="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-cur">
+            <img src="https://demo.jantra.co/AssetsHome/img/cur3.png" class="img-fluid" alt="">
             <div class="portfolio-info">
-              <h4>Card 3</h4>
-              <p>Card</p>
-              <a href="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-8.jpg" data-gall="portfolioGallery" class="venobox preview-link" title="Card 3"><i class="bx bx-plus"></i></a>
+              <h4>เก็บสายชาร์</h4>
+              <p>เก็บสายชาร์</p>
+              <a href="https://demo.jantra.co/AssetsHome/img/cur3.png" data-gall="portfolioGallery" class="venobox preview-link" title="เก็บสายชาร์"><i class="bx bx-plus"></i></a>
               <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
             </div>
           </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <img src="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
-            <div class="portfolio-info">
-              <h4>Web 3</h4>
-              <p>Web</p>
-              <a href="https://demo.jantra.co/AssetsHome/img/portfolio/portfolio-9.jpg" data-gall="portfolioGallery" class="venobox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
-              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-            </div>
-          </div>
+ 
 
         </div>
 
