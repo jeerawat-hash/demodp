b89ead1c329ad38d7dab08ab99ba2a50c8350372
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-08-17 15:16:33 +0700

    add

--- application/views/homepages/servicesc.php
+++ application/views/homepages/servicesc.php
@@ -39,6 +39,86 @@
 
 
 
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>ตัวอย่างบริการ กายบำบัด</h2>
+        </div>
+
+        <div class="row" data-aos="fade-up">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+              <li data-filter="*" class="filter-active">ทั้งหมด</li>
+              <li data-filter=".filter-com">ตัวอย่าง1</li>
+              <li data-filter=".filter-lat">ตัวอย่าง2</li>
+              <li data-filter=".filter-ec">ตัวอย่าง3</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up">
+
+
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-com">
+            <img src="https://demo.jantra.co/AssetsHome/img/servicecom1.png" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4>แผนกคอมพิวเตอร์ ภาพที่ 1</h4>
+              <p>แผนกคอมพิวเตอร์</p>
+              <a href="https://demo.jantra.co/AssetsHome/img/servicecom1.png" data-gall="portfolioGallery" class="venobox preview-link" title="แผนกคอมพิวเตอร์ ภาพที่ 1"><i class="bx bx-plus"></i></a>
+              <a href="#" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
+
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-lat">
+            <img src="https://demo.jantra.co/AssetsHome/img/servicelat1.png" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4>แผนกเย็บหนัง ภาพที่ 1</h4>
+              <p>แผนกเย็บหนัง</p>
+              <a href="https://demo.jantra.co/AssetsHome/img/servicelat1.png" data-gall="portfolioGallery" class="venobox preview-link" title="แผนกเย็บหนัง ภาพที่ 1"><i class="bx bx-plus"></i></a>
+              <a href="#" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
+
+
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-lat">
+            <img src="https://demo.jantra.co/AssetsHome/img/servicelat2.png" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4>แผนกเย็บหนัง ภาพที่ 2</h4>
+              <p>แผนกเย็บหนัง</p>
+              <a href="https://demo.jantra.co/AssetsHome/img/servicelat2.png" data-gall="portfolioGallery" class="venobox preview-link" title="แผนกเย็บหนัง ภาพที่ 2"><i class="bx bx-plus"></i></a>
+              <a href="#" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
+ 
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-ec">
+            <img src="https://demo.jantra.co/AssetsHome/img/serviceec1.png" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4>แผนกอิเล็กทรอนิกส์ ภาพที่ 1</h4>
+              <p>แผนกอิเล็กทรอนิกส์</p>
+              <a href="https://demo.jantra.co/AssetsHome/img/serviceec1.png" data-gall="portfolioGallery" class="venobox preview-link" title="แผนกอิเล็กทรอนิกส์ ภาพที่ 1"><i class="bx bx-plus"></i></a>
+              <a href="#" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
+ 
+
+
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+ 
 
 
 
