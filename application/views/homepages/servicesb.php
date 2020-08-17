b1f58d74d3cb542b1ebb5d90ee4ba92b9ac9df79
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-08-17 15:09:33 +0700

    add

--- application/views/homepages/servicesb.php
+++ application/views/homepages/servicesb.php
@@ -85,6 +85,141 @@
             </article>
 
 
+            <article class="entry" data-aos="fade-up">
+              <div class="entry-img">
+                <img src="https://demo.jantra.co/AssetsHome/img/blog-4.jpg" alt="" class="img-fluid">
+              </div>
+              <h2 class="entry-title">
+              <a href="blog-single.html">กรณีบัตรชำรุด หรือสูญหาย</a>
+              </h2>
+              <div class="entry-content">
+                <p> <br>1.บัตรประจำตัวคนพิการ (บัตรเดิมที่หมดอายุ)
+                    <br>2.บัตรประชาชน หรือบัตรประจำตัวข้าราชการ หรือสูติบัตรสำหรับบุคคลอายุต่ำกว่า 7 ปี หรือหนังสือรับรองการเกิดตามแบบที่กรมการปกครองกำหนด
+                    <br>3.ทะเบียนบ้านของคนพิการ
+                    <br>4.รูปถ่ายขนาด 1 นิ้ว ถ่ายไว้ไม่เกิน 6 เดือน จำนวน 2 รูป
+                    <br>5.สำเนาบัตรประชาชน และสำเนาทะเบียนบ้านของผู้ดูแล (ถ้ามี)
+
+                    </p>
+                <div class="read-more">
+                  <a href="https://demo.jantra.co/index.php/Home/projectdetail">รายละเอียด</a>
+                </div>
+              </div>
+            </article>
+
+
+            <article class="entry" data-aos="fade-up">
+              <div class="entry-img">
+                <img src="https://demo.jantra.co/AssetsHome/img/blog-4.jpg" alt="" class="img-fluid">
+              </div>
+              <h2 class="entry-title">
+              <a href="blog-single.html">กรณีขอมีบัตรประตัวแทนคนพิการ</a>
+              </h2>
+              <div class="entry-content">
+                <p> <br>1.เอกสารขอมีบัตรประจำตัวคนพิการ
+                    <br>2.บัตรประชาชนของผู้ยื่นคำขอแทนคนพิการ
+                    <br>3.ใบมอบอำนาจ หรือหลักฐานอื่นที่แสดงให้เห็นว่าได้รับมอบอำนาจจากคนพิการที่มีส่วนเกี่ยวของกับคนพิการเนื่องจากเป็นผู้ปกครอง ผู้พิทักษ์ ผู้อนุบาล หรือผู้ดูแล คนพิการแล้วแต่กรณี
+
+                    </p>
+                <div class="read-more">
+                  <a href="https://demo.jantra.co/index.php/Home/projectdetail">รายละเอียด</a>
+                </div>
+              </div>
+            </article>
+
+
+            <article class="entry" data-aos="fade-up">
+              <div class="entry-img">
+                <img src="https://demo.jantra.co/AssetsHome/img/blog-4.jpg" alt="" class="img-fluid">
+              </div>
+              <h2 class="entry-title">
+              <a href="blog-single.html">กรณีลงชื่อผู้ดูแลคนพิการต้องมีคุณสมบัติ ดังนี้</a>
+              </h2>
+              <div class="entry-content">
+                <p> <br>1.เป็นญาติโดยสายเลือด อยู่ทะเบียนบ้านเดียวกัน
+                    <br>2.เป็นญาติโดยสายเลือด แต่อยู่คนละทะเบียนบ้าน ต้องมีผู้รับรองว่าเป็นผู้ดูแลคนพิการ
+                    <br>3.ไม่ได้เป็นญาติโดยสายเลือด แต่อยู่ทะเบียนบ้านเดียวกัน ต้องมีผู้รับรองว่าเป็นผู้ดูแลคนพิการ
+                    <br>4.เป็นบุคคลอื่นที่ไม่ใช่ญาติ และอยู่คนละทะเบียนบ้าน ม่สามารถลงเป็นผู้ดูแลได้ เว้นแต่มีผู้รับรองและสอบปากคำกับนายทะเบียน
+
+                    </p>
+                <div class="read-more">
+                  <a href="https://demo.jantra.co/index.php/Home/projectdetail">รายละเอียด</a>
+                </div>
+              </div>
+            </article>
+
+
+
+            <article class="entry" data-aos="fade-up">
+              <div class="entry-img">
+                <img src="https://demo.jantra.co/AssetsHome/img/blog-4.jpg" alt="" class="img-fluid">
+              </div>
+              <h2 class="entry-title">
+              <a href="blog-single.html">การให้บริการรับยื่นเอกสารการกู้ยืมเงินเพื่อเป็นทุนประกอบอาชีพของคนพิการ</a>
+              </h2>
+              <div class="entry-content">
+                <p> การกู้ยืมเงินเพื่อเป็นทุนประกอบอาชีพของคนพิการ โดยวงเงินกู้ รายละไม่เกิน 60,000บาท ทั้งนี้หากประสงค์จะขอกู้ยืมเงินเกินกว่าวงเงินที่กำหนด ให้มีการพิจารณาเป็นรายๆ ไป รายละไม่เกิน 120,000 บาท
+                    </p>
+                <div class="read-more">
+                  <a href="https://demo.jantra.co/index.php/Home/projectdetail">รายละเอียด</a>
+                </div>
+              </div>
+            </article>
+
+
+
+            <article class="entry" data-aos="fade-up">
+              <div class="entry-img">
+                <img src="https://demo.jantra.co/AssetsHome/img/blog-4.jpg" alt="" class="img-fluid">
+              </div>
+              <h2 class="entry-title">
+              <a href="blog-single.html">เอกสารประกอบการกู้ยืมเงิน</a>
+              </h2>
+              <div class="entry-content">
+                <p>
+                  <br>1.บัตรประชาน
+                  <br>2.บัตรประจำตัวคนพิการ
+                  <br>3.ทะเบียนบ้าน
+                  <br>4.รูปถ่ายเต็มตัวคนพิการ ขนาด 4x๖ นิ้ว
+                  <br>5.แผนผังที่อยู่อาศัย และแผนผังสถานประกอบอาชีพ
+                  <br>6.สำเนาหนังสือเช่าบ้าน (กรณีเช่าบ้านอยู่)
+                  <br>7.สำเนาบัตรประชาชน และสำเนาทะเบียนบ้านของผู้ค้ำประกัน
+                  <br>8.หนังสือรับรองเงินเดือนของผู้ค้ำประกัน
+                  <br>9.ใบรับรองแพทย์ (กรณีคนพิการมีสภาพความพิการร้ายแรงและผู้ดูแลคนพิการเป็นผู้ดูแลคนพิการเป็นผู้ขอกู้แทนคนพิการ)
+                  <br>10.ประมาณการค่าใช้จ่ายในการประกอบอาชีพที่ขอกู้ (กรณีกู้ยืมรายบุคคล) หรือโครงการประกอบอาชีพ (กรณีกู้ยืมรายกลุ่ม)
+
+                </p>
+                <div class="read-more">
+                  <a href="https://demo.jantra.co/index.php/Home/projectdetail">รายละเอียด</a>
+                </div>
+              </div>
+            </article>
+
+
+            <article class="entry" data-aos="fade-up">
+              <div class="entry-img">
+                <img src="https://demo.jantra.co/AssetsHome/img/blog-4.jpg" alt="" class="img-fluid">
+              </div>
+              <h2 class="entry-title">
+              <a href="blog-single.html">คุณสมบัติผู้กู้ยืมเงินเพื่อประกอบอาชีพรายบุคคล</a>
+              </h2>
+              <div class="entry-content">
+                <p>
+                  <br>1.มีบัตรประจำตัวคนพิการ
+                  <br>2.มีความจำเป็นในการขอรับการสนับสนุนเงินกู้ยืมเพื่อประกอบอาชีพภายในจังหวัดที่ยื่นเรื่องกู้
+                  <br>3.มีความสามารถในการประกอบอาชีพในเรื่องขอรับการสนับสนุน
+                  <br>4.บรรลุนิติภาวะ (อายุ 20 ปี หรือบรรลุนิติภาวะโดยการสมรส)
+                  <br>5.มีชื่อในทะเบียนและมีถิ่นที่อยู่ถาวรในท้องที่ที่ยื่นคำขอกู้ไม่น้อยกว่า 90 วัน
+                  <br>6.ไม่มีประวัติสียหายในการกู้ยืมเงินจากกองทุน เว้นแต่ได้ดำเนินการแก้ไขมาแล้ว ไม่น้อยกว่า 1 ปี
+                  <br>7.กรณีมีหนี้สินอยู่กับกองทุนต้องได้รับชำระหนี้มาแล้ว ไม่น้อยกว่าร้อยละ 60 ของวงเงินกู้ยืมทั้งหมด
+                  <br>8.มีความสามารถชำระคืนเงินกู้ยืมได้ และมีบุคคลที่น่าเชื่อถือได้เป็นผู้ค้ำประกัน
+                </p>
+                <div class="read-more">
+                  <a href="https://demo.jantra.co/index.php/Home/projectdetail">รายละเอียด</a>
+                </div>
+              </div>
+            </article>
+
+
 
           </div><!-- End blog entries list -->
  
