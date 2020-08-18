e4415f36b7497679d3d11d7f985ce5cfa9d5cfc9
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-08-18 09:21:19 +0700

    add

---
+++ application/controllers/Main.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+ 
+	function __construct()
+	{
+	
+ 		parent::__construct(); 
+ 		/*$this->load->library("session");
+		$this->load->model("Member_model");
+
+		if ( $this->session->userdata("memberID") == "" ) {
+			
+			redirect("signin");
+			$this->session->sess_destroy();
+		}
+		*/
+		
+
+	}
+
+
+	public function index()
+	{ 
+		$this->load->view('main'); 
+	}
+	
+
+
+}
